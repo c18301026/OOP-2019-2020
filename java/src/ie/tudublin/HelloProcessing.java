a74b9aaec9ec5182de16cb9bf3ee763e27b515b9
c18301026 <c18301026@mytudublin.ie>
2020-02-03 12:52:14 +0000

    Week 2 code

--- java/src/ie/tudublin/HelloProcessing.java
+++ java/src/ie/tudublin/HelloProcessing.java
@@ -30,11 +30,49 @@ public class HelloProcessing extends PApplet
 	
 	public void draw()
 	{	
-		
-		background(0);
+		// PROCESSING STUFF!!!
+
+		background(255, 0, 0);
 		noStroke();
-		fill(255);
-		ellipse(x, mouseY, 50, 50);		
-		x ++;
+
+		fill(255, 255, 0); // RGB 
+		ellipse(250, 250, 250, 250);
+
+		fill(0, 255, 255);
+		triangle(100,350,400,350,250,100);
+
+		fill(200);
+		ellipse(250, 250, 125, 75);
+
+		fill(0);
+		ellipse(250, 250, 50, 50);
+
+		/*
+		// Background colour
+		// background(255); // 0 = black, 255 = white
+		background(0, 255, 0); // Red, Green, Blue
+
+		// outline of things
+		stroke(0, 0, 255);
+
+		// How to draw a line
+		line(10, 10, 100, 100); // four parameters, x1, y1, x2, y2
+		point(50, 50); // x, y
+
+		fill(0, 255, 255);
+
+		noStroke();
+
+		// draw circular shape
+		ellipse(100, 200, 100, 50); // 4 parameters: centrex, centrey, width, height
+
+		fill(2550, 0, 0);
+
+		// draw rectangle
+		rect(20, 150, 90, 10); // 4 parameters: topleftx, toplefty, width, height
+
+		// draw triangle
+		triangle(150, 90, 200, 100, 100, 250);
+		*/
 	}
 }
